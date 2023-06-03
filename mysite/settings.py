5a9e6478005c4aac28adb9f785305e345aefad43
nano <nano@nanonoMacBook-Air.local>
2023-06-03 22:14:35 +0900

    appの設定

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework', #追加
+    'app', #追加
+    'accounts' #追加
 ]
 
 MIDDLEWARE = [
